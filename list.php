04392d195e7944a780568bfd84a478d56c243966
auliahasna929 <42725478+auliahasna929@users.noreply.github.com>
2018-10-05 18:05:05 +0700

    Update list.php

--- list.php
+++ list.php
@@ -1,30 +1,44 @@
 <?php
-require_once("db.php");git 
+require_once("db.php");
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
 <table border=1>
-    <thead>
-        <th>Nama</th>
-        <th>Nim</th>
-        <th>Tanggal Lahir</th>
-    </thead>
-    <tbody>
-<?php
-$sql    = "SELECT * FROM siswa";
-$result = mysqli_query($conn, $sql);
+	<thead>
+		
+		<th>Nim</th>
+		<th>Nama</th>
+		<th>Prodi</th>
+		<th>fakultas</th>
+		<th>aksi</th>
+	</thead>
+	<tbody>
+		<?php
+		$sql = "SELECT * FROM mhs";
+		$result = mysqli_query($conn, $sql);
+$row = mysqli_num_rows($result);
 
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "<tr>";
-        echo "<td>" . $row["nama"]. "</td>"; 
-        echo "<td>" . $row["nim"]. "</td>";
-        echo "<td>" . $row["tgl_lahir"]. "</td>";
-        echo "</tr>";
-    }
-} else {
-    echo "0 results";
-}
-mysqli_close($conn);
-?> 
-    </tbody>
+		if ($row > 0 ) {
+			//output data of each row
+			while($row = mysqli_fetch_array($result)){
+				$id = $row["nim"];
+				echo "<tr>";
+				echo "<td>" . $row["nim"] . "</td";
+				echo "<td>" . $row["nama"] . "</td";
+				echo "<td>" . $row["prodi"] . "</td";
+				echo "<td>" . $row["fakultas"] . "</td";
+				echo "<td>" ."<a href ='newform.php?nim=$id'>edit</a>|a href ='delete.php'?nim$id'>hapus</a>"."</td>";
+				echo "</tr>";
+			}
+		}else {
+			echo "<tr><td colspan='4' align='center'>0 result.</td></tr>";
+		}
+?>
+	</tbody>
 </table>
+</body>
+</html>
